c27bd717d470150be8540ac72a6b3d73f8f64e60
Sanka Vollereii <arigowok05@gmail.com>
2025-01-20 01:38:50 +0700

    Update fnjdason.js

--- fnjdason.js
+++ fnjdason.js
@@ -1,7 +1,9 @@
 {
   "allowed_ips": [
-    "209.38.87.70", //Sanka Vollerei
-    "170.64.171.188", //Sanka Vollerei
+    "209.38.87.70",
+    //Sanka Vollerei
+    "170.64.171.188", 
+    //Sanka Vollerei
     "",
     ""
   ]
